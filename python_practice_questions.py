67afaa45ca17970a3d46c112118400dcef767185
Aspiring Python Developer | Learning SQL & DSA <vijaymishrauk70@gmail.com>
2026-05-19 20:16:22 +0530

    Day 1 — Python Practice (Beginner)

---
+++ python_practice_questions.py
@@ -0,0 +1,64 @@
+#Create a variable name and store your name.
+name ="ram"
+#print the name variable 
+print(name)
+
+
+#Create variables age, city, and college and print them.
+age =25
+city = "delhi"
+college ="abc college"
+print(age)
+print(city)
+print(college)
+
+#store two number and print their sum.
+num1 = 10
+num2 = 20
+sum = num1 +num2 
+print(sum)
+
+#swap two variables and print the result.
+a =5
+b=10
+temp =a
+a=b
+b=temp
+print("after swapping a:",a)
+
+
+#take user input and print a greeting message with the input name.
+user_name =input("enter your naem:")
+print("Hello " +user_name+" Welcome to python programming!")
+
+
+#check data type of a integer  float ,string and boolean. values.
+int_var = 10
+float_var = 3.14
+string_var= "hello"
+boolean_var =True
+print(type(int_var))
+print(type(float_var))
+print(type(string_var))
+print(type(boolean_var))
+
+# Convert integer into string.
+num_str = '123'
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
+
+
+#Convert string "100" into integer.
+str_num = 100
+int_num = int(str_num)
+print(int_num)
+#conbert string "3.14"into float.
+str_float ="3.14"
+float_num = float(str_float)
+print(float_num)
+
+
+
+
+
